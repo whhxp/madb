84ca4f90224e524456be8479b2f96052af12312a
hh <whhxp@foxmail.com>
2017-07-18 18:15:42 +0800

    提取出了没有括号的合金的配比

--- alloy2elements/alloy2elements.py
+++ alloy2elements/alloy2elements.py
@@ -4,10 +4,11 @@
 
 import math
 from datetime import datetime,timedelta
+import re
 
 
 extension = ".csv"
-path = "/Users/huihuanwu/Documents/PolyU/work/ME/machine_learning/aa/"
+path = ""
 in_file_name ="MGS_G"
 in_file_name+= extension
 out_file_name = "out" + extension
@@ -25,48 +26,77 @@ class alloy2elements(object):
         # Step 1: Load trajectories
         fr = open(path + in_file_name, 'r')
         fr.readline()  # skip the header
-        traj_data = fr.readlines()
+        alloy_data = fr.readlines()
         fr.close()
-        print(traj_data[0])
+        print(alloy_data[0])
 
         # Step 2: Create a dictionary to store travel time for each route per time window
         travel_times = {}  # key: route_id. Value is also a dictionary of which key is the start time for the time window and value is a list of travel times
-        for i in range(len(traj_data)):
-            each_traj = traj_data[i].replace('"', '').split(',')
-            intersection_id = each_traj[0]
-            tollgate_id = each_traj[1]
 
-            route_id = intersection_id + '-' + tollgate_id
-            if route_id not in travel_times.keys():
-                travel_times[route_id] = {}
+        data1={}
+        for i in range(len(alloy_data)):
+            elements=[]
+            data=[]
+            dict_elements = {}
+            each_alloy = alloy_data[i].replace('"', '').split(',')
+            alloyContent = each_alloy[0]
+            Gbas = each_alloy[7]
 
-            trace_start_time = each_traj[3]
-            trace_start_time = datetime.strptime(trace_start_time, "%Y-%m-%d %H:%M:%S")
-            time_window_minute = math.floor(trace_start_time.minute / 20) * 20
-            start_time_window = datetime(trace_start_time.year, trace_start_time.month, trace_start_time.day,
-                                         trace_start_time.hour, time_window_minute, 0)
-            tt = float(each_traj[-1])  # travel time
+            print(alloyContent)
+            print("Gbas=%f" % float(Gbas))
 
-            if start_time_window not in travel_times[route_id].keys():
-                travel_times[route_id][start_time_window] = [tt]
+            pattern2=re.compile(r'\(')
+
+            p1=re.compile(r'''([A-Z][a-z]?(?:[1-9]\d*|0)+(?:\.\d+)?)''')
+            pattern_word=re.compile(r'([A-Z][a-z]?)')
+            pattern_float=re.compile(r'((?:[1-9]\d*|0)+(?:\.\d+)?)')
+            match2 = pattern2.search(alloyContent)
+            if (match2):
+                print("pattern2 result = %s" % match2.group())
             else:
-                travel_times[route_id][start_time_window].append(tt)
+                for m in p1.finditer(alloyContent):
+                    each_element=m.group()
+                    # print(each_element)
+                    matchWord=pattern_word.search(each_element)
+                    match1=pattern_float.search(each_element)
+                    if (matchWord):
+                        elements.append(matchWord.group())
+                    if(match1):
+                            data.append(float(match1.group()))
+                dict_elements = dict(zip(elements, data))
+                print(dict_elements)
+            print("---end----")
+            # route_id = intersection_id + '-' + tollgate_id
+            # if route_id not in travel_times.keys():
+            #     travel_times[route_id] = {}
+            #
+            # trace_start_time = each_traj[3]
+            # trace_start_time = datetime.strptime(trace_start_time, "%Y-%m-%d %H:%M:%S")
+            # time_window_minute = math.floor(trace_start_time.minute / 20) * 20
+            # start_time_window = datetime(trace_start_time.year, trace_start_time.month, trace_start_time.day,
+            #                              trace_start_time.hour, time_window_minute, 0)
+            # tt = float(each_traj[-1])  # travel time
+            #
+            # if start_time_window not in travel_times[route_id].keys():
+            #     travel_times[route_id][start_time_window] = [tt]
+            # else:
+            #     travel_times[route_id][start_time_window].append(tt)
 
         # Step 3: Calculate average travel time for each route per time window
-        fw = open(out_file_name, 'w')
-        fw.writelines(','.join(['"intersection_id"', '"tollgate_id"', '"time_window"', '"avg_travel_time"']) + '\n')
-        for route in travel_times.keys():
-            route_time_windows = list(travel_times[route].keys())
-            route_time_windows.sort()
-            for time_window_start in route_time_windows:
-                time_window_end = time_window_start + timedelta(minutes=20)
-                tt_set = travel_times[route][time_window_start]
-                avg_tt = round(sum(tt_set) / float(len(tt_set)), 2)
-                out_line = ','.join(['"' + route.split('-')[0] + '"', '"' + route.split('-')[1] + '"',
-                                     '"[' + str(time_window_start) + ',' + str(time_window_end) + ')"',
-                                     '"' + str(avg_tt) + '"']) + '\n'
-                fw.writelines(out_line)
-        fw.close()
+        # fw = open(out_file_name, 'w')
+        # fw.writelines(','.join(['"intersection_id"', '"tollgate_id"', '"time_window"', '"avg_travel_time"']) + '\n')
+        # for route in travel_times.keys():
+        #     route_time_windows = list(travel_times[route].keys())
+        #     route_time_windows.sort()
+        #     for time_window_start in route_time_windows:
+        #         time_window_end = time_window_start + timedelta(minutes=20)
+        #         tt_set = travel_times[route][time_window_start]
+        #         avg_tt = round(sum(tt_set) / float(len(tt_set)), 2)
+        #         out_line = ','.join(['"' + route.split('-')[0] + '"', '"' + route.split('-')[1] + '"',
+        #                              '"[' + str(time_window_start) + ',' + str(time_window_end) + ')"',
+        #                              '"' + str(avg_tt) + '"']) + '\n'
+        #         fw.writelines(out_line)
+        # fw.close()
 
     def writeCSV(self):
         print("write csv file")
